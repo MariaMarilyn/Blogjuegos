54aab2ae78c6c8a3e14a74d68fdf1ed5be4abc1b
juanmacod <132533683+juanmacod@users.noreply.github.com>
2023-07-27 01:15:21 -0300

    URLS
    Agregue las URLS igualmente hay que modificarlas si o si con la ruta de los templates...

---
+++ BlogDjuegos/apps/comentarios/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+]
--- BlogDjuegos/apps/usuarios/forms.py
+++ BlogDjuegos/apps/usuarios/forms.py
@@ -1,24 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import Usuario
 
 
-class RegistroForm(UserCreationForm):
-    email = forms.EmailField(label='Correo', required= True)
-    First_name = forms.CharField(label='nombre', required=True)
-    Last_name = forms.CharField(label='apellido', required= True)
+class RegistroForm(forms.ModelForm):
+    email = forms.EmailField(label='Correo', required=True)
+    first_name = forms.CharField(label='Nombre', required=True)
+    last_name = forms.CharField(label='Apellido', required=True)
     password1 = forms.CharField(
-        label='contraseña', widget=forms.PasswordInput, required= True)
+        label='Contraseña', widget=forms.PasswordInput, required=True)
     password2 = forms.CharField(
-        label='contraseña2', widget=forms.PasswordInput, required= True)
+        label='Confirmar contraseña', widget=forms.PasswordInput, required=True)
 
-
-class meta:
-    model = Usuario
-    fields = [
-        'First_name',
-        'Last_name',
-        'username',
-        'password1',
-        'password2',
-    ]
+    class Meta:
+        model = Usuario
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2',
+        ]
